09a75852a0867511e80d078f04ddfa644dc2e36c
vnsasson <42601574+vnsasson@users.noreply.github.com>
2020-08-03 09:15:20 +0300

    Add files via upload

--- preprocessing.m
+++ preprocessing.m
@@ -1,14 +1,18 @@
-close all; clc; 
+close all; clc; clear all
 
 fs = 500;
 
-% ALLEEG(2) = EEG;
-% CURRENTSET = 2;
-
+% uncomment in the first time
 [ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
 
+% uncomment in the second time
+%ALLEEG(2) = EEG;
+%CURRENTSET = 2;
+
+
+
 %% 1-  Loading the data (all channels)
-EEG = pop_loadbv('C:\Users\sasso\Desktop\Studies\Project\B- Brain activity during reading\data\', 'Reading_screen_6.2019_31.12.19.vhdr', [], [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63]);
+EEG = pop_loadbv('C:\Users\sasso\Desktop\Studies\Project\B- Brain activity during reading\data\', 'Reading_paper_6.2019_31.12.19.vhdr', [], [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63]);
 EEG.setname='Original';
 EEG = eeg_checkset( EEG );
 [ALLEEG EEG CURRENTSET] = eeg_store(ALLEEG, EEG); % save modifications

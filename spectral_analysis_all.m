d810794b4a80ca2e0c8c3de65a853bbe3b68d299
vnsasson <42601574+vnsasson@users.noreply.github.com>
2020-12-17 13:13:20 +0200

    Add files via upload

--- spectral_analysis_all.m
+++ spectral_analysis_all.m
@@ -2,17 +2,23 @@ clc; close all; clear all;
 
 [ALLEEG EEG CURRENTSET ALLCOM] = eeglab;  
 
+% import set
 setDir = 'C:\\Users\\sasso\\Desktop\\Studies\\Project\\B- Brain activity during reading\\datasets\\after_removing'; 
 
 myFiles = dir(fullfile(setDir,'*.set')); %gets all set files in struct
 
-freqReg = 5; % alpha, beta,...
-brainReg = 7; % frontal, left, right,...
+freqReg = 5; % alpha, beta,... (number of frequency regions)
+brainReg = 7; % frontal, left, right,... (number of brain regions)
+filesNum = size(myFiles);
+filesNum = filesNum(1);
 
-Mat = zeros(freqReg*size(myFiles),brainReg);
+Mat = zeros(freqReg*filesNum,brainReg);
 
 for k = 1:length(myFiles)
   filename = myFiles(k).name;
   filepath = myFiles(k).folder;
-  Mat = spectral_analysis_func(ALLEEG,filename,filepath);  
+  Mat = spectral_analysis_func(ALLEEG,filename,filepath,Mat,k);  
 end
+
+
+

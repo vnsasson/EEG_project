b7c3d51b77b7c668775dfa73cdb80545d12ececc
vnsasson <42601574+vnsasson@users.noreply.github.com>
2021-05-06 11:17:00 +0300

    Add files via upload
    1 subject

--- Divide2Mat.m
+++ Divide2Mat.m
@@ -4,36 +4,37 @@ clc; close all; clear all;
 
 % Constants
 
+setDir = 'C:\\Users\\sasso\\Desktop\\Studies\\Project\\B- Brain activity during reading\\datasets\\8\\';
+
 SubjectsNum = 14;
 TimeSize = 18;
 FreqSize = 45;
 
-
 % Excel matrix of dipole's order
-
-OrdersMat = xlsread('orderMat.xlsx','AB2:A9');
-OrdersMat = fliplr(OrdersMat);
+OrdersMat = fliplr(xlsread('orderMat.xlsx','AB2:A9'));
 
 
 %% Load datasets
 
-EEG = pop_loadset('filename',{'11.7_paper.set','11.7_screen.set','14.7_paper.set','14.7_screen.set','15.10_paper.set','15.10_screen.set','15.7_paper.set','15.7_screen.set','17.10_paper.set','17.10_screen.set','19.8_paper.set','19.8_screen.set','21.8_paper.set','21.8_screen.set','24.12_paper.set','24.12_screen.set','24.9_paper.set','24.9_screen.set','28.7_paper.set','28.7_screen.set','4.7_paper.set','4.7_screen.set','5.12_paper.set','5.12_screen.set','5.8_paper.set','5.8_screen.set','8.12_paper.set','8.12_screen.set'},'filepath','C:\\Users\\sasso\\Desktop\\Studies\\Project\\B- Brain activity during reading\\datasets\\8\\');
+EEG = pop_loadset('filename',{'11.7_paper.set','11.7_screen.set','14.7_paper.set','14.7_screen.set','15.10_paper.set','15.10_screen.set','15.7_paper.set','15.7_screen.set','17.10_paper.set','17.10_screen.set','19.8_paper.set','19.8_screen.set','21.8_paper.set','21.8_screen.set','24.12_paper.set','24.12_screen.set','24.9_paper.set','24.9_screen.set','28.7_paper.set','28.7_screen.set','4.7_paper.set','4.7_screen.set','5.12_paper.set','5.12_screen.set','5.8_paper.set','5.8_screen.set','8.12_paper.set','8.12_screen.set'},'filepath',setDir);
 
 
-%% Left Frontal -> Right Frontal
+%% Fill 2 Matrices (Paper,Screen) for 2 chosen brain areas
 
-LFRFpaper = zeros(FreqSize,TimeSize,SubjectsNum);
-LFRFscreen = zeros(FreqSize,TimeSize,SubjectsNum);
+Paper = zeros(FreqSize,TimeSize,SubjectsNum);
+Screen = zeros(FreqSize,TimeSize,SubjectsNum);
+
+% brain regins
 LF=1; RF=2; LP=3; RP=4; LO=5; RO=6; LT=7; LR=8;
 
 for subj = 1 : SubjectsNum
-    [LFRFpaper,LFRFscreen] = Fill2Mat(EEG,OrdersMat,LFRFpaper,LFRFscreen,subj);
+    [Paper,Screen] = Fill2Mat(EEG,OrdersMat,Paper,Screen,subj,LF,LP);
 end
 
-%% P - test
+%% P - test for 2 chosen brain areas
 
-[stats1, df1, pvals1, surrog1] = statcond(LFRFpaper, LFRFscreen, 'mode','bootstrap','naccu',10000 );
-[h2, crit_p1, adj_ci_cvrg1, adj_p1]=fdr_bh(pvals1);   %fdr correction
+[stats1, df1, pvals1, surrog1] = statcond(Paper, Screen, 'mode','bootstrap','naccu',10000 );
+%[h2, crit_p1, adj_ci_cvrg1, adj_p1]=fdr_bh(pvals1);   %fdr correction
 
 
 

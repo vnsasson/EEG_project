5836c9fc3388af9c10e887123edcbe195a0e9197
vnsasson <42601574+vnsasson@users.noreply.github.com>
2021-05-10 18:53:37 +0300

    Add files via upload

--- Divide2Mat.m
+++ Divide2Mat.m
@@ -2,136 +2,122 @@ clc; close all; clear all;
 
 [ALLEEG EEG CURRENTSET ALLCOM] = eeglab; 
 
-% Constants
+% Dataset's directory
+% old setDir = 'C:\\Users\\sasso\\Desktop\\Studies\\Project\\B- Brain activity during reading\\datasets\\8\\';
+setDir = 'C:\\Users\\Sasson\\Desktop\\Studies\\EEG_project\\datasets\\8\\';
+% Excel matrix of dipole's order
+OrdersMat = fliplr(xlsread('orderMat.xlsx','AB2:A9'));
 
-setDir = 'C:\\Users\\sasso\\Desktop\\Studies\\Project\\B- Brain activity during reading\\datasets\\8\\';
+% Constants
 
 SubjectsNum = 14;
 TimeSize = 18;
 FreqSize = 45;
+BrainRegNum = 8;
+ConnectNum = 64;
+p_val = 0.05;
 
-% Excel matrix of dipole's order
-OrdersMat = fliplr(xlsread('orderMat.xlsx','AB2:A9'));
+%% set regions
 
+Regions = cell(1,BrainRegNum);
+% Regions{1} = 'Left Frontal'; Regions{2} = 'Right Frontal'; Regions{3} = 'Left Partial'; 
+% Regions{4} = 'Right Partial'; Regions{5} = 'Left Occipital'; Regions{6} = 'Right Occipital';
+% Regions{7} = 'Left Temporal'; Regions{8} = 'Right Temporal';
+
+Regions{1} = 'LF'; Regions{2} = 'RF'; Regions{3} = 'LP'; 
+Regions{4} = 'RP'; Regions{5} = 'LO'; Regions{6} = 'RO';
+Regions{7} = 'LT'; Regions{8} = 'RT';
 
 %% Load datasets
 
 EEG = pop_loadset('filename',{'11.7_paper.set','11.7_screen.set','14.7_paper.set','14.7_screen.set','15.10_paper.set','15.10_screen.set','15.7_paper.set','15.7_screen.set','17.10_paper.set','17.10_screen.set','19.8_paper.set','19.8_screen.set','21.8_paper.set','21.8_screen.set','24.12_paper.set','24.12_screen.set','24.9_paper.set','24.9_screen.set','28.7_paper.set','28.7_screen.set','4.7_paper.set','4.7_screen.set','5.12_paper.set','5.12_screen.set','5.8_paper.set','5.8_screen.set','8.12_paper.set','8.12_screen.set'},'filepath',setDir);
 
 
-%% Fill 2 Matrices (Paper,Screen) for 2 chosen brain areas
+%% Fill 2 Matrices (Paper,Screen) for all brain areas
+% The Big matrices includes:
+%                             The first dimension represent the whole
+%                             connections between two brain regions.
+% For every connection has a 3-D matrix of time*freq for each subject                            
 
-Paper = zeros(FreqSize,TimeSize,SubjectsNum);
-Screen = zeros(FreqSize,TimeSize,SubjectsNum);
+BigPaper = zeros(ConnectNum,FreqSize,TimeSize,SubjectsNum);
+BigScreen = zeros(ConnectNum,FreqSize,TimeSize,SubjectsNum);
 
-% brain regins
-LF=1; RF=2; LP=3; RP=4; LO=5; RO=6; LT=7; LR=8;
+for from = 1 : BrainRegNum
+    for to = 1 : BrainRegNum
 
-for subj = 1 : SubjectsNum
-    [Paper,Screen] = Fill2Mat(EEG,OrdersMat,Paper,Screen,subj,LF,LP);
+        Paper = zeros(FreqSize,TimeSize,SubjectsNum);
+        Screen = zeros(FreqSize,TimeSize,SubjectsNum);
+
+        for subj = 1 : SubjectsNum
+            [Paper,Screen] = Fill2Mat(EEG,OrdersMat,Paper,Screen,subj,from,to);
+        end
+        BigPaper((from-1)*BrainRegNum+to,:,:,:) = Paper;
+        BigScreen((from-1)*BrainRegNum+to,:,:,:) = Screen;
+    end
 end
 
-%% P - test for 2 chosen brain areas
 
-[stats1, df1, pvals1, surrog1] = statcond(Paper, Screen, 'mode','bootstrap','naccu',10000 );
-%[h2, crit_p1, adj_ci_cvrg1, adj_p1]=fdr_bh(pvals1);   %fdr correction
+%%
 
+Adj_P1 = zeros(FreqSize,TimeSize,ConnectNum);
 
+for i = 1 : ConnectNum
+    
+    Single_paper = single(BigPaper(i,:,:,:));
+    Single_screen = single(BigScreen(i,:,:,:));
+    Res_cell = cell(1,2);
+    Res_cell{1} = Single_paper;
+    Res_cell{2} = Single_screen;
+    
+    [stats1, df1, pvals1, surrog1] = statcond(Res_cell, 'mode','bootstrap','naccu',10000 );
+    [h2, crit_p1, adj_ci_cvrg1, adj_p1]=fdr_bh(pvals1);   %fdr correction
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    Adj_P1(:,:,i) = adj_p1;
+    
+end
 
 
+%% Transform the p_val matrix to (1/0) matrix
+% 1 = for p-val < 0.05
+% 0 = for p_val > 0.05
+
+ZeroOne = zeros(FreqSize,TimeSize,ConnectNum);
+
+for i = 1 : FreqSize
+    for j = 1 : TimeSize
+        for k = 1 : ConnectNum
+            if Adj_P1(i,j,k) <= p_val
+                ZeroOne(i,j,k) = 1;
+            end
+        end
+    end
+end
 
 
+%% plotting all connectivity matrices
 
+time = EEG(1).CAT.Conn.winCenterTimes;
+freq = 1:FreqSize;
 
+k=1;
+figure;
+for i = 1 : ConnectNum
+    
+    from = fix(i/BrainRegNum)+1;
+    to = mod(i,BrainRegNum);
+    
+    if mod(i,BrainRegNum)==0
+        from = from-1;
+        to = BrainRegNum;
+    end
+    
+    if from == to
+        continue;
+    end
+    
+    subplot(8,7,k);
+    imagesc(time,freq,ZeroOne(:,:,i));
+    title(['From ' Regions{from} ' to ' Regions{to}]);
+    k=k+1;
 
-
-
-
-
-% %% Left Frontal -> Right Frontal for first subject (11.7)
-% 
-% Mat = EEG(1).CAT.Conn.dDTF08(OrdersMat(1,1),OrdersMat(2,1),:,:); 
-% Mat = reshape(Mat,124,18); 
-% LFRFpaper(:,:,1) = Mat(1:45,:); %takes [1,45] Hz
-% 
-% Mat = EEG(2).CAT.Conn.dDTF08(OrdersMat(1,2),OrdersMat(2,2),:,:); 
-% Mat = reshape(Mat,124,18); 
-% LFRFscreen(:,:,1) = Mat(1:45,:); %takes [1,45] Hz
-% 
-% %% Left Frontal -> Left Partial for first subject
-% 
-% Mat = EEG(1).CAT.Conn.dDTF08(OrdersMat(1,1),OrdersMat(3,1),:,:); 
-% Mat = reshape(Mat,124,18); 
-% LFLPpaper(:,:,1) = Mat(1:45,:); %takes [1,45] Hz
-% 
-% Mat = EEG(2).CAT.Conn.dDTF08(OrdersMat(1,2),OrdersMat(3,2),:,:); 
-% Mat = reshape(Mat,124,18); 
-% LFLPscreen(:,:,1) = Mat(1:45,:); %takes [1,45] Hz
-% 
-% %% Left Frontal -> Right Frontal for second subject (14.7)
-% 
-% Mat = EEG(3).CAT.Conn.dDTF08(OrdersMat(1,3),OrdersMat(2,3),:,:); 
-% Mat = reshape(Mat,124,18); 
-% LFRFpaper(:,:,2) = Mat(1:45,:); %takes [1,45] Hz
-% 
-% Mat = EEG(4).CAT.Conn.dDTF08(OrdersMat(1,4),OrdersMat(2,4),:,:); 
-% Mat = reshape(Mat,124,18); 
-% LFRFscreen(:,:,2) = Mat(1:45,:); %takes [1,45] Hz
-% 
-% %% Left Frontal -> Left Partial for second subject (14.7)
-% 
-% Mat = EEG(3).CAT.Conn.dDTF08(OrdersMat(1,3),OrdersMat(3,3),:,:); 
-% Mat = reshape(Mat,124,18); 
-% LFLPpaper(:,:,2) = Mat(1:45,:); %takes [1,45] Hz
-% 
-% Mat = EEG(4).CAT.Conn.dDTF08(OrdersMat(1,4),OrdersMat(3,4),:,:); 
-% Mat = reshape(Mat,124,18); 
-% LFLPscreen(:,:,2) = Mat(1:45,:); %takes [1,45] Hz
-
-
-% %%  plotting
-% 
-% time = EEG(1).CAT.Conn.winCenterTimes;
-% freq = 1:45;
-% 
-% figure;
-% surf(time,freq,LFRFpaper(:,:,2));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-% %% 
-% 
-% for k = 1:length(myFiles)
-%   filename = myFiles(k).name;
-%   filepath = myFiles(k).folder;
-%   %sift_to_matrix_func(ALLEEG,filename,filepath); 
-%   
-%   fill_matrices(ALLEEG,filename,filepath,OrdersMat,LFRFpaper,LFRFscreen);
-% end
+end
--- Fill2Mat.m
+++ Fill2Mat.m
@@ -1,15 +1,15 @@
 function [Paper,Screen] = Fill2Mat(EEG,OrdersMat,Paper,Screen,subj,from,to)
 
-TimeSize = 18;
-FreqSize = 45;
+    TimeSize = 18;
+    FreqSize = 45;
 
-Mat = EEG(subj).CAT.Conn.dDTF08(OrdersMat(from,subj*2-1),OrdersMat(to,subj*2-1),:,:); 
-Mat = reshape(Mat,length(Mat),TimeSize); % change matrix dimension
-Paper(:,:,subj) = Mat(1:FreqSize,:); %takes [1,45] Hz
+    Mat = EEG(subj).CAT.Conn.dDTF08(OrdersMat(from,subj*2-1),OrdersMat(to,subj*2-1),:,:); 
+    Mat = reshape(Mat,length(Mat),TimeSize); % change matrix dimension
+    Paper(:,:,subj) = Mat(1:FreqSize,:); %takes [1,45] Hz
 
-Mat = EEG(subj+1).CAT.Conn.dDTF08(OrdersMat(from,subj*2),OrdersMat(to,subj*2),:,:); 
-Mat = reshape(Mat,length(Mat),TimeSize);
-Screen(:,:,subj) = Mat(1:FreqSize,:); 
+    Mat = EEG(subj+1).CAT.Conn.dDTF08(OrdersMat(from,subj*2),OrdersMat(to,subj*2),:,:); 
+    Mat = reshape(Mat,length(Mat),TimeSize);
+    Screen(:,:,subj) = Mat(1:FreqSize,:); 
 
 end
 
